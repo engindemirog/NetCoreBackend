8ead46c58d786a882455530cff8a251654e6be8b
mtyuksel <yukselmtevfik@gmail.com>
2021-05-14 17:16:28 +0300

    Servisler temel CRUD işlemleri için Generic hale getirildi. (IServiceBase)

---
+++ Business/Abstract/IServiceBase.cs
@@ -0,0 +1,12 @@
+﻿using Core.Utilities.Results;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IServiceBase<T>
+    {
+        IResult Add(T entity); 
+        IResult Update(T entity);
+        IResult Delete(T entity);
+    }
+}
