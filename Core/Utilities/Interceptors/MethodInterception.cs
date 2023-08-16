97c48be0f34c02e33ab0a6241e4b96a5c9866b06
Fâtih <43666342+fatih-dogmus@users.noreply.github.com>
2023-08-16 08:05:43 +0300

    Update MethodInterception.cs
    hata verirse zaten catch bloğuna düşeceğinden ek bir değişken tutmaya gerek yok

--- Core/Utilities/Interceptors/MethodInterception.cs
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -13,24 +13,16 @@ namespace Core.Utilities.Interceptors
         protected virtual void OnSuccess(IInvocation invocation) { }
         public override void Intercept(IInvocation invocation)
         {
-            var isSuccess = true;
             OnBefore(invocation);
             try
             {
                 invocation.Proceed();
+                OnSuccess(invocation);
             }
             catch (Exception e)
             {
-                isSuccess = false;
                 OnException(invocation,e);
                 throw;
-            }
-            finally
-            {
-                if (isSuccess)
-                {
-                    OnSuccess(invocation);
-                }
             }
             OnAfter(invocation);
         }
